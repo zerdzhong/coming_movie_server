02253de633994f0d71c8847535b8d25bb838fe68
zerdzhong <zhongzhendongchn@gmail.com>
2018-03-30 14:12:55 +0800

    Request logger

---
+++ logger.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+// Logger 请求日志打印
+func Logger(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		inner.ServeHTTP(w, r)
+		log.Printf("%s\t%s\t%s\t%s", r.Method, r.RequestURI, name, time.Since(start))
+	})
+}
